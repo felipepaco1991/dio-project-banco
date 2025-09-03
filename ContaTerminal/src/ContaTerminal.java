4eda11bccf7092d4d98419d4b7ff551769d8c2c3
Felipe Paço <lipeklc26@gmail.com>
2025-09-03 00:21:37 -0300

    Refactor ContaTerminal for improved readability and maintainability

--- ContaTerminal/src/ContaTerminal.java
+++ ContaTerminal/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero = 0;
+        String agencia = "";   
+        String nomeCliente = "";
+        double saldo = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor, digite o número da agência: ");
+        agencia = scanner.nextLine();
+        System.out.println("Por favor, digite o número da conta: ");
+        numero = scanner.nextInt();
+        scanner.nextLine(); // Consumir a quebra de linha pendente
+        System.out.println("Por favor, digite o nome do cliente: ");
+        nomeCliente = scanner.nextLine();
+        System.out.println("Por favor, digite o saldo da conta (use ponto para separar os centavos): ");
+        saldo = scanner.nextDouble();
+        scanner.close();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
